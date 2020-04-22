de43217fe46885323da49555e0769c12257d3c47
Y-Dan Bui <ydanbui@gmail.com>
2020-04-21 20:07:21 -0700

    added sass support

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
     entry: {
-        index: './src/index.js'
+        index: './src/scripts/index.js'
     },
     output: {
         path: path.resolve(__dirname, 'public/scripts') ,
