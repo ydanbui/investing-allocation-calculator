32b4ff26d85cc5daa7f261c5f6b0f33b546ae820
Y-Dan Bui <ydanbui@gmail.com>
2020-04-22 19:14:00 -0700

    automatically fill in allocation to sum to 100%

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -64,7 +64,6 @@ const symbolInputEls = document.querySelectorAll('.input__symbol')
 symbolInputEls.forEach(input => {
     input.addEventListener('keydown', e => {
         if (e.keyCode < 65 && e.keyCode !== 8 && e.keyCode !== 9 && e.keyCode !== 13 || e.keyCode > 90) {
-            console.log('fire')
             e.preventDefault()
         }
     })
@@ -76,18 +75,73 @@ moneyInputEls.forEach(input => {
     new AutoNumeric(input, {
         emptyInputBehavior: "zero",
         minimumValue: "0"
-        // unformatOnSubmit: true
-        // outputFormat: "number"
     })
 })
 
 // Format allocation fields using Autonumeric
 const allocationInputEls = document.querySelectorAll('.input__allocation')
-allocationInputEls.forEach(input => {
-    new AutoNumeric(input, {
+const allocationAutoNumArr = []
+const numStocks = allocationInputEls.length
+
+allocationInputEls.forEach((input, index) => {
+    allocationAutoNumArr[index] = new AutoNumeric(input, {
         decimalPlaces: 0,
         maximumValue: "99",
         minimumValue: "1"
     })
-})
 
+    // If there are just two stocks, changing allocaiton of one changes the other accordingly to sum to 100
+    if (numStocks === 2) {
+        // If this input is the last stock, change the first allocation
+        if (index === numStocks - 1) {
+            input.addEventListener('keyup', e => {
+                // If there are just two stocks the remaining allocation is just 100 - the current allocation
+                const remainingAllocation = 100 - input.value
+                // Set the allocation of the previous stock to add to 100
+                allocationAutoNumArr[index - 1].set(remainingAllocation)
+            })
+        } else {
+            input.addEventListener('keyup', e => {
+                // If there are just two stocks the remaining allocation is just 100 - the current allocation
+                const remainingAllocation = 100 - input.value
+                allocationAutoNumArr[index + 1].set(remainingAllocation)
+            
+            })
+        }
+    } else {
+        // If there are more than 2 stock
+        const allocationInputsArr = Array.from(allocationInputEls)
+
+        // If it's the last stock, change the previous allocation so it sums to 100 when we change the last allocation
+        if (index === numStocks - 1) {
+            input.addEventListener('keyup', e => {
+                const sumOfPrevAllocations = allocationInputsArr.reduce((total, allocInput, ind) => {
+                    if (ind < numStocks - 2) {
+                        return total + parseFloat(allocInput.value)
+                    } else {
+                        return total
+                    }
+                }, 0)
+
+                const remainingAllocation = 100 - input.value - sumOfPrevAllocations
+                // Set the allocation of the previous stock to add to 100
+                allocationAutoNumArr[index - 1].set(remainingAllocation)
+            })
+        } else if (index === numStocks - 2){
+            // If it's the second to last stock, change the last stock's allocation so they sum to 100
+            input.addEventListener('keyup', e => {
+                const sumOfPrevAllocations = allocationInputsArr.reduce((total, allocInput, ind) => {
+                    if (ind < numStocks - 1) {
+                        return total + parseFloat(allocInput.value)
+                    } else {
+                        return total
+                    }
+                }, 0)
+
+                const remainingAllocation = 100 - sumOfPrevAllocations
+                // Set the allocation of the next stock to add to 100
+                allocationAutoNumArr[index + 1].set(remainingAllocation)
+            })
+        }
+    }
+})
