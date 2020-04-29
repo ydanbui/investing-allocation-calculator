0ff3a761a7e876b14711070d26112fb8099d41ef
Y-Dan Bui <ydanbui@gmail.com>
2020-04-29 01:47:32 -0700

    cleanup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,7 +9,6 @@ import validator from "validator"
 // Load the Visualization API and the corechart package.
 google.charts.load('current', {'packages':['corechart']});
 
-
 const maxAmountToInvestEl = document.querySelector('#maxAmountToInvestEl')
 
 addStockInputGroup()
